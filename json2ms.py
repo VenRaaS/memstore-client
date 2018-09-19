46b1637e637e8f5c89f79d8611695fff0d8d6084
root <root@instance-group-venapi-39b5.c.pchomeecvenraas.internal>
2018-09-19 17:26:17 +0800

    [json2ms.py] compliance with python 2.6

--- json2ms.py
+++ json2ms.py
@@ -47,11 +47,11 @@ if '__main__' == __name__:
 
     parser = argparse.ArgumentParser()
     parser.add_argument("src_fp", help="source file path")
-    parser.add_argument("-c", default="{}".format(jkey_c), help="source json key represents code name, default: {}".format(jkey_c))
-    parser.add_argument("-t", default="{}".format(jkey_t), help="source json key represents table/mode name, default: {}".format(jkey_t))
-    parser.add_argument("-i", default="{}".format(jkey_i), help="source json key represents gid/category/item/rule id, default: {}".format(jkey_i))
-    parser.add_argument("-v", default="{}".format(jkey_v), help="source json key represents rule/recomd list, default: {}".format(jkey_v))
-    parser.add_argument("-ttl", default=86400, help="live time of keys, default: {} seconds".format(86400))
+    parser.add_argument("-c", default="{0}".format(jkey_c), help="source json key represents code name, default: {0}".format(jkey_c))
+    parser.add_argument("-t", default="{0}".format(jkey_t), help="source json key represents table/mode name, default: {0}".format(jkey_t))
+    parser.add_argument("-i", default="{0}".format(jkey_i), help="source json key represents gid/category/item/rule id, default: {0}".format(jkey_i))
+    parser.add_argument("-v", default="{0}".format(jkey_v), help="source json key represents rule/recomd list, default: {0}".format(jkey_v))
+    parser.add_argument("-ttl", default=86400, help="live time of keys, default: {0} seconds".format(86400))
     args = parser.parse_args()
 
     jkey_c = args.c 
@@ -59,9 +59,9 @@ if '__main__' == __name__:
     jkey_i = args.i
     jkey_v = args.v
     if args.ttl: expire_sec_mpv.value = args.ttl
-    logging.info('combo key: ${}.${}.${}'.format(jkey_c, jkey_t, jkey_i))
-    logging.info('value key: {}'.format(jkey_v))
-    logging.info('ttl: {}'.format(args.ttl))
+    logging.info('combo key: ${0}.${1}.${2}'.format(jkey_c, jkey_t, jkey_i))
+    logging.info('value key: {0}'.format(jkey_v))
+    logging.info('ttl: {0}'.format(args.ttl))
     exit()
   
     logging.info('{} counting ...'.format(args.src_fp))
