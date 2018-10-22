dab9355527309126c6dbc68eab6478eaff8002ed
eric0329 <ericyang0329@gmail.com>
2018-10-22 19:46:49 +0800

    [json2ms.py] forms json file parsers as callback functions

--- json2ms.py
+++ json2ms.py
@@ -25,10 +25,44 @@ expire_sec_mpv = Value('i', 0)
 
 
 ##
+## pipelining
+## see https://github.com/andymccurdy/redis-py#pipelines,
+##     https://redis.io/topics/pipelining,
+##     https://redis.io/topics/mass-insert
+##
+def rds_pipe_worker(tuple_list):
+    #-- disable the atomic nature of a pipeline
+    #   see https://github.com/andymccurdy/redis-py#pipelines
+    with rds.pipeline(transaction=False) as pipe:        
+        for args, k, v, trim_s2s in tuple_list:
+            if RedisCommand.append == args.cmd_redis:
+                pipe.append(k, '{_v},'.format(_v=v))
+            elif RedisCommand.set == args.cmd_redis:
+                pipe.set(k, v)
+            elif RedisCommand.get == args.cmd_redis:
+                pipe.get(k)
+            elif RedisCommand.rpush == args.cmd_redis:
+                pipe.rpush(k, v)
+            elif RedisCommand.lpush == args.cmd_redis:
+                pipe.lpush(k, v)
+
+            if args.ttl:
+                pipe.expire(k, args.ttl)
+
+            if args.ltrim:
+                pipe.ltrim(k, args.ltrim[0], args.ltrim[1])
+            elif trim_s2s:
+                pipe.ltrim(k, trim_s2s[0], trim_s2s[1])
+
+        pipe.execute()
+        print 'pipelining {:,} rows'.format(len(tuple_list))
+
+
+##
 ## simulates the linux command, e.g. tail -F [file]
 ## see http://man7.org/linux/man-pages/man1/tail.1.html
 ##
-def tail_file(args, cbf, seconds_sleep=3):
+def tail_file(args, parser_cbf, seconds_sleep=3):
     fname = args.src_fp
     cur_f = open(fname, 'r')
     cur_ino = os.fstat(cur_f.fileno()).st_ino
@@ -43,7 +77,7 @@ def tail_file(args, cbf, seconds_sleep=3):
                 lines = cur_f.readlines(10 * 1024 * 1024)
 #                print lines
                 if 0 < len(lines):
-                    cbf(args, lines)
+                    parser_cbf(args, fname, 0, lines)
 
                 if not lines:
                     logging.info('EOF')
@@ -69,11 +103,11 @@ def tail_file(args, cbf, seconds_sleep=3):
             cur_f.close()
 
 
-def tail_sync_file(rds, args):
+def tail_sync_file(args):
     tail_file(args, weblog_td_parser)
 
 
-def weblog_td_parser(args, lines):
+def weblog_td_parser(args, fn, cntbase, lines):
     tuple_list = []
     try:
         for l in lines:
@@ -126,101 +160,59 @@ def weblog_td_parser(args, lines):
         logging.error(e, exc_info=True)
 
 
-def goccmod_parser(args, lines):
-        tuple_list = []
-
-        for i_line, l in enumerate(f):
-            try:
-                j = json.loads(l)
-
-                if not jkey_c in j:
-                    logging.error('{} is not found at line:{} in {}'.format(jkey_c, i_line, fn))
-                    continue
-                if not jkey_t in j:
-                    logging.error('{} is not found at line:{} in {}'.format(jkey_t, i_line, fn))
-                    continue
-                if not jkey_k in j:
-                    logging.error('{} is not found at line:{} in {}'.format(jkey_k, i_line, fn))
-                    continue
-
-                if jkeys_vals:
-                    for vk in jkeys_vals:
-                        if not vk in j:
-                            logging.error('{} is not found at line:{} in {}'.format(vk, i_line, fn))
-                            continue
-
-#                            k = '{c}_{ic}.{t}.{k}.{i}'.format(c=j[jkey_c], ic=args.index_cat, t=j[jkey_t], k=jkey_k, i=j[jkey_k])
-                k = '/{c}_{ic}/{t}/_search?q={k}:{i}'.format(c=j[jkey_c], ic=args.index_cat, t=j[jkey_t], k=jkey_k, i=j[jkey_k])
-                if jkeys_vals:
-                    d = {}
-                    for vk in jkeys_vals:
-                        d[vk] = j[vk]
-                else:
-                    d = j
-                v = json.dumps(d)
-
-                trim_s2s = None
-                if args.ltrim:
-                    trim_s2s = (args.ltrim[0], args.ltrim[1])
-                
-                tuple_list.append((args, k, v, trim_s2s))
-
-                size = i_line + 1
-                if 1 == size or size % (60 * 1000) == 0 or size_src <= size:
-                    rds_pipe_worker(tuple_list)
-                    tuple_list = []
-                    logging.info('{:.0f} {:.0f}%'.format(size, size / size_src * 100))
-
-            except Exception as e:
-                logging.error(e, exc_info=True)
-
-
-
-def rds_pipe_worker(tuple_list):
-    #-- disable the atomic nature of a pipeline
-    #   see https://github.com/andymccurdy/redis-py#pipelines
-    with rds.pipeline(transaction=False) as pipe:        
-        for args, k, v, trim_s2s in tuple_list:
-            if RedisCommand.append == args.cmd_redis:
-                pipe.append(k, '{_v},'.format(_v=v))
-            elif RedisCommand.set == args.cmd_redis:
-                pipe.set(k, v)
-            elif RedisCommand.get == args.cmd_redis:
-                pipe.get(k)
-            elif RedisCommand.rpush == args.cmd_redis:
-                pipe.rpush(k, v)
-            elif RedisCommand.lpush == args.cmd_redis:
-                pipe.lpush(k, v)
-
-            if args.ttl:
-                pipe.expire(k, args.ttl)
-
-            if args.ltrim:
-                pipe.ltrim(k, args.ltrim[0], args.ltrim[1])
-            elif trim_s2s:
-                pipe.ltrim(k, trim_s2s[0], trim_s2s[1])
-
-        pipe.execute()
-        print 'pipelining {:,} rows'.format(len(tuple_list))
-
-
-##
-## pipelining
-## see https://github.com/andymccurdy/redis-py#pipelines,
-##     https://redis.io/topics/pipelining,
-##     https://redis.io/topics/mass-insert
-##
-def pipe_sync_file(rds, args) :
+def goccmod_parser(args, fn, cntbase, lines):
     jkey_c = args.c 
     jkey_t = args.t
     jkey_k = args.k
     jkeys_vals = args.valkeys
-    logging.info('combo key: ${0}.${1}.${2}'.format(jkey_c, jkey_t, jkey_k))
-    logging.info('value key: {0}'.format(jkeys_vals))
-    logging.info('ttl: {0}'.format(args.ttl))
-    logging.info('command: {0}'.format(args.cmd_redis))
-    logging.info('deamon mode: {0}'.format(args.deamon))
 
+    tuple_list = []
+    for i_line, l in enumerate(lines):
+        try:
+            j = json.loads(l)
+
+            if not jkey_c in j:
+                logging.error('{} is not found at line:{} in {}'.format(jkey_c, i_line+cntbas, fn))
+                continue
+            if not jkey_t in j:
+                logging.error('{} is not found at line:{} in {}'.format(jkey_t, i_line+cntbase, fn))
+                continue
+            if not jkey_k in j:
+                logging.error('{} is not found at line:{} in {}'.format(jkey_k, i_line+cntbase, fn))
+                continue
+
+            if jkeys_vals:
+                for vk in jkeys_vals:
+                    if not vk in j:
+                        logging.error('{} is not found at line:{} in {}'.format(vk, i_line, fn))
+                        continue
+
+#                k = '{c}_{ic}.{t}.{k}.{i}'.format(c=j[jkey_c], ic=args.index_cat, t=j[jkey_t], k=jkey_k, i=j[jkey_k])
+            k = '/{c}_{ic}/{t}/_search?q={k}:{i}'.format(c=j[jkey_c], ic=args.index_cat, t=j[jkey_t], k=jkey_k, i=j[jkey_k])
+
+            d = {}
+            if jkeys_vals:
+                for vk in jkeys_vals:
+                    if args.lowervalkeys:
+                        vk = vk.lower() 
+                    d[vk] = j[vk]
+            else:
+                d = j
+            v = json.dumps(d)
+
+            trim_s2s = None
+            if args.ltrim:
+                trim_s2s = (args.ltrim[0], args.ltrim[1])
+            
+            tuple_list.append((args, k, v, trim_s2s))
+
+        except Exception as e:
+            logging.error(e, exc_info=True)
+
+    rds_pipe_worker(tuple_list)
+
+
+def pipe_file(args, parser_cbf):
     state_files = FilesState(args.src_fp, args.deamon)
     new_state_files = state_files 
     while True:
@@ -240,61 +232,44 @@ def pipe_sync_file(rds, args) :
                     size_src = i
                     pass
             size_src += 1.0
-            logging.info('{} has {:.0f} records'.format(fn, size_src))
-
+            logging.info('{} has {:,.0f} records'.format(fn, size_src))
+            
             with open(fn, 'r') as f:
-                tuple_list = []
+                size = 0
+                while True:
+                    lines = f.readlines(60 * 1024 * 1024)
+                    if 0 < len(lines):
+                        parser_cbf(args, fn, size, lines)
+                    else:
+                        break
 
-                for i_line, l in enumerate(f):
-                    try:
-                        j = json.loads(l)
-
-                        if not jkey_c in j:
-                            logging.error('{} is not found at line:{} in {}'.format(jkey_c, i_line, fn))
-                            continue
-                        if not jkey_t in j:
-                            logging.error('{} is not found at line:{} in {}'.format(jkey_t, i_line, fn))
-                            continue
-                        if not jkey_k in j:
-                            logging.error('{} is not found at line:{} in {}'.format(jkey_k, i_line, fn))
-                            continue
-
-                        if jkeys_vals:
-                            for vk in jkeys_vals:
-                                if not vk in j:
-                                    logging.error('{} is not found at line:{} in {}'.format(vk, i_line, fn))
-                                    continue
-
-#                            k = '{c}_{ic}.{t}.{k}.{i}'.format(c=j[jkey_c], ic=args.index_cat, t=j[jkey_t], k=jkey_k, i=j[jkey_k])
-                        k = '/{c}_{ic}/{t}/_search?q={k}:{i}'.format(c=j[jkey_c], ic=args.index_cat, t=j[jkey_t], k=jkey_k, i=j[jkey_k])
-                        if jkeys_vals:
-                            d = {}
-                            for vk in jkeys_vals:
-                                d[vk] = j[vk]
-                        else:
-                            d = j
-                        v = json.dumps(d)
-
-                        trim_s2s = None
-                        if args.ltrim:
-                            trim_s2s = (args.ltrim[0], args.ltrim[1])
-                        
-                        tuple_list.append((args, k, v, trim_s2s))
-
-                        size = i_line + 1
-                        if 1 == size or size % (60 * 1000) == 0 or size_src <= size:
-                            rds_pipe_worker(tuple_list)
-                            tuple_list = []
-                            logging.info('{:.0f} {:.0f}%'.format(size, size / size_src * 100))
-
-                    except Exception as e:
-                        logging.error(e, exc_info=True)
+                    size += len(lines)
+###                    if 1 == size or size % (60 * 1000) == 0 or size_src <= size:
+                    logging.info('{:,.0f} {:,.0f}%'.format(size, size / size_src * 100))
 
         if not args.deamon:
             break
         
         time.sleep(SLEEP_FOR_FILE_CHANGE_DETECTION_IN_SEC)
         new_state_files = FilesState(args.src_fp, args.deamon)
+
+
+def pipe_sync_file(args):
+    if IndexCategory.gocc == args.index_cat or \
+        IndexCategory.mod == args.index_cat:
+        jkey_c = args.c 
+        jkey_t = args.t
+        jkey_k = args.k
+        jkeys_vals = args.valkeys
+        logging.info('combo key: ${0}.${1}.${2}'.format(jkey_c, jkey_t, jkey_k))
+        logging.info('value key: {0}'.format(jkeys_vals))
+        logging.info('ttl: {0}'.format(args.ttl))
+        logging.info('command: {0}'.format(args.cmd_redis))
+        logging.info('deamon mode: {0}'.format(args.deamon))
+        pipe_file(args, goccmod_parser)
+
+    elif IndexCategory.weblogtd == args.index_cat:
+        pipe_file(args, weblog_td_parser)
 
 
 class FilesState:
@@ -320,8 +295,7 @@ from enum import Enum
 class IndexCategory(Enum):
     gocc = 'gocc'
     mod = 'mod'
-    opp = 'opp'
-    oua = 'oua'
+    weblogtd = 'weblogtd'
 
     def __str__(self):
         return self.value
@@ -347,6 +321,7 @@ if '__main__' == __name__:
     parser.add_argument("-t", default="{0}".format(jkey_t), help="source json key for table/mode name, default: {0}".format(jkey_t))
     parser.add_argument("-k", default="{0}".format(jkey_k), help="source json key for key/gid/item id, default: {0}".format(jkey_k))
     parser.add_argument("-v", "--valkeys", action='append', help="source json key for value/rule content, default: all")
+    parser.add_argument("-lv", "--lowervalkeys", action='store_false', help="lower value/rule json key")
     parser.add_argument("-ttl", "--ttl", type=int, default=259200, help='live time of keys')
     parser.add_argument('index_cat', type=IndexCategory, choices=list(IndexCategory), help="index category")
     parser.add_argument('cmd_redis', type=RedisCommand, choices=list(RedisCommand), help="redis commands")
@@ -376,6 +351,6 @@ if '__main__' == __name__:
             logging.error(e, exc_info=True)
  
 #    print args
-    args.func(rds, args)
+    args.func(args)
 
  
